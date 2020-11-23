4d96aa81134f0b5b900bf8463e1f6c707d373300
大阳 <Feliz337@outlook.com>
2020-11-23 22:33:17 +0800

    setup docker

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:14.15.1-alpine3.10
+WORKDIR /app
+ADD package*.json /app/package.json
+RUN yarn config set registry https://registry.npm.taobao.org
+RUN yarn
+ADD . /app
+RUN yarn run build
+EXPOSE 3000
+CMD ["yarn","run","start:prod"]
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,15 @@ import { AppModule } from "./app.module";
 import { WsAdapter } from "@nestjs/platform-ws";
 import { RedisIoAdapter } from "./ws/RedisIoAdapter";
 import dotenv from "dotenv"
+import * as fs from "fs";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: fs.readFileSync(process.env.SSL_PRIVATE_KEY_PATH),
+      cert: fs.readFileSync(process.env.SSL_CERT_PATH)
+    }
+  });
   app.enableCors({ origin: true, credentials: true });
   // app.useWebSocketAdapter(new RedisIoAdapter(app));
   await app.listen(3000);
